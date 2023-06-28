b85213b2b35f987597c2e088c9a3997effee9ea3
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-06-28 19:36:24 +0300

    Решение

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "1234 № 44444444";
+        post.patronumic = "Петрович";
+        post.phone = "+7 (800)-555-35-55";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+
+        post.birthday.day = 15;
+        post.birthday.month = 5;
+        post.birthday.year = 1964;
+    }
 }
